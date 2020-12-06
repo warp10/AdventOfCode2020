36415872bcbf3b2f2b622cef32a37a5f0072a1b6
Andrea Colangelo <andreacolangelo@openforce.it>
2020-12-06 12:15:15 +0100

    Add O(n) version for day 1, puzzle 1

--- day_01/puzzle_1/solution.py
+++ day_01/puzzle_1/solution.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 
-def main(filename):
+def main_unoptimized(filename):
+    # This is the first, unoptimized, O(n^2) version
     with open(filename) as f:
         res = [int(item.strip()) for item in f.readlines()]
         for item1 in res:
             for item2 in res:
                 if item1 + item2 == 2020:
                     return item1*item2
+
+
+def main(filename):
+    with open(filename) as f:
+        res = [int(item.strip()) for item in f.readlines()]
+        res.sort()
+        start = 0
+        end = -1
+        while True:
+            if res[start] + res[end] == 2020:
+                return res[start] * res[end]
+            elif res[start] + res[end] < 2020:
+                start += 1
+            elif res[start] + res[end] > 2020:
+                end -= 1
 
 
 if __name__ == '__main__':
