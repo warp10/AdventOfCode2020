80d303798a099b9804630a27b6eb8f0eb81521b3
Andrea Colangelo <andreacolangelo@openforce.it>
2020-12-08 22:26:08 +0100

    7-1: refactor to include quantities too in the main data structure

--- day_07/puzzle_1/solution.py
+++ day_07/puzzle_1/solution.py
@@ -5,26 +5,27 @@ data = {}
 
 def load_lines(lines):
     """Structure of data:
-    {key_color: [color1, color2, ...}
+    {key_color: [(color1, qty1), (color2, qty2), ...]}
     """
     global data
     for line in lines:
         key, content = line.split(' bags contain ')
         if content == 'no other bags.':
-            data[key] = ['no bags']
+            data[key] = [('no bags', 0)]
         else:
             values = []
             raw_values = content.split(', ')
             for raw_value in raw_values:
-                _, raw_color = raw_value.split(' ', 1)
+                qty, raw_color = raw_value.split(' ', 1)
                 color, _ = raw_color.rsplit(' ', 1)
-                values.append(color)
+                values.append((color, qty))
             data[key] = values
 
 
-def is_valid_bag(colors):
+def is_valid_bag(values):
     # memoization would have been nice here, but it was not worth writing it
     # given the small input
+    colors = [value[0] for value in values]
     if 'shiny gold' in colors:
         return True
     elif 'no bags' in colors:
@@ -38,7 +39,7 @@ def main(filename):
         lines = [line.strip() for line in f.readlines()]
         load_lines(lines)
 
-    return sum(is_valid_bag(colors) for colors in data.values())
+    return sum(is_valid_bag(values) for values in data.values())
 
 
 if __name__ == '__main__':
