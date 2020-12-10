aff53672ee94da5dd8aa70992dfdc1aac211044c
Andrea Colangelo <andreacolangelo@openforce.it>
2020-12-10 23:50:46 +0100

    Typo in 10-1 test

--- day_10/puzzle_1/test.py
+++ day_10/puzzle_1/test.py
@@ -5,7 +5,7 @@ import unittest
 from solution import main
 
 
-class TestPuzzle09_1(unittest.TestCase):
+class TestPuzzle10_1(unittest.TestCase):
     def test_main(self):
         self.assertEqual(main('test_input_1.txt'), 35)
         self.assertEqual(main('test_input_2.txt'), 220)
